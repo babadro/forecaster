2fa35fe2a0a0872b4d6f5ef2731649f66432b01c
Andrei Petrovich <babadro@gmail.com>
2023-07-05 21:29:06 +0200

    swagger in progress

--- internal/core/forecaster/service.go
+++ internal/core/forecaster/service.go
@@ -16,6 +16,12 @@ func NewService(db DB) *Service {
 
 type DB interface {
 	GetPollByID(ctx context.Context, id int32) (bot.Poll, error)
+	CreatePoll(ctx context.Context, poll bot.Poll) (bot.Poll, error)
+	CreateOption(ctx context.Context, option bot.Option) (bot.Option, error)
+	UpdatePoll(ctx context.Context, poll bot.Poll) (bot.Poll, error)
+	UpdateOption(ctx context.Context, option bot.Option) (bot.Option, error)
+	DeletePoll(ctx context.Context, id int32) error
+	DeleteOption(ctx context.Context, id int32) error
 }
 
 func (s *Service) GetPollByID(ctx context.Context, id int32) (bot.Poll, error) {
