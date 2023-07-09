de4fe7f0b1278f9aedcaa266ba55ee2def4d9ecf
andreipetrovich <babadro@gmail.com>
2023-07-09 16:41:38 +0200

    series in db schema

--- migrations/001_init.up.sql
+++ migrations/001_init.up.sql
@@ -1,11 +1,21 @@
+CREATE TABLE forecaster.series (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL
+);
+
 CREATE TABLE forecaster.polls (
     id SERIAL PRIMARY KEY,
+    series_id INT NOT NULL DEFAULT 0,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     start timestamp with time zone NOT NULL,
     finish timestamp with time zone NOT NULL,
     created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL
+    updated_at timestamp with time zone NOT NULL,
+    FOREIGN KEY (series_id) REFERENCES forecaster.series(id) ON DELETE CASCADE
 );
 
 CREATE TABLE forecaster.options (
