fcba2b8eaaced03de2e4b401efae8097beb9dc74
Andrei Petrovich <babadro@gmail.com>
2023-08-02 07:57:34 +0200

    processUpdate in progress

--- internal/core/forecaster/telegram/telegram.go
+++ internal/core/forecaster/telegram/telegram.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/babadro/forecaster/internal/helpers"
 	models "github.com/babadro/forecaster/internal/models/swagger"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/rs/zerolog"
@@ -46,9 +48,6 @@ func (s *Service) ProcessTelegramUpdate(logger *zerolog.Logger, upd tgbotapi.Upd
 
 	result := s.processTGUpdate(ctx, upd)
 
-	// todo remove
-	result.msgText = upd.Message.Text
-
 	if result.msgText != "" {
 		msg := tgbotapi.NewMessage(upd.Message.Chat.ID, result.msgText)
 		if _, sendErr := s.tgBot.Send(msg); sendErr != nil {
@@ -60,7 +59,8 @@ func (s *Service) ProcessTelegramUpdate(logger *zerolog.Logger, upd tgbotapi.Upd
 }
 
 type processTGResult struct {
-	msgText string
+	msgText        string
+	inlineKeyboard tgbotapi.InlineKeyboardMarkup
 }
 
 func (s *Service) processTGUpdate(ctx context.Context, upd tgbotapi.Update) processTGResult {
@@ -83,7 +83,6 @@ func (s *Service) processTGUpdate(ctx context.Context, upd tgbotapi.Update) proc
 			}
 
 			poll, err := s.db.GetPollByID(ctx, int32(pollID))
-			_ = poll // todo remove
 
 			if err != nil {
 				l.Error().Msgf("unable to get poll by id: %v\n", err)
@@ -92,8 +91,73 @@ func (s *Service) processTGUpdate(ctx context.Context, upd tgbotapi.Update) proc
 					msgText: fmt.Sprintf("oops, can't find poll with id %d", pollID),
 				}
 			}
+
+			return processTGResult{
+				msgText:        txtMsg(poll),
+				inlineKeyboard: keyboardMarkup(poll),
+			}
 		}
 	}
 
 	return processTGResult{}
+}
+
+func keyboardMarkup(poll models.PollWithOptions) tgbotapi.InlineKeyboardMarkup {
+	length := len(poll.Options)
+	rowsCount := length / 8
+
+	if length%8 > 0 {
+		rowsCount++
+	}
+
+	rows := make([][]tgbotapi.InlineKeyboardButton, rowsCount)
+
+	for i := range poll.Options {
+		rowIdx := i / 8
+		rows[rowIdx] = append(rows[rowIdx], tgbotapi.InlineKeyboardButton{
+			Text:         strconv.Itoa(i + 1),
+			CallbackData: helpers.Ptr(""),
+		})
+	}
+
+	var keyboard tgbotapi.InlineKeyboardMarkup
+	keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, rows...)
+	return keyboard
+}
+
+func txtMsg(poll models.PollWithOptions) string {
+	var sb strings.Builder
+	sb.WriteString(pollToString(poll))
+	sb.WriteString("\n")
+	if time.Now().Unix() >= int64(time.Time(poll.Finish).Unix()) {
+		sb.WriteString("This poll is expired!\n")
+		// todo
+		//if poll.OutcomeOptionId > 0 {
+		//	option := poll.Options[poll.OutcomeOptionId]
+		//	sb.WriteString(fmt.Sprintf("Outcome option is: #%d %q", poll.OutcomeOptionId+1, option.Title))
+		//}
+	}
+	// todo
+	//if votedOptionID > -1 {
+	//	sb.WriteString(fmt.Sprintf("You have already voted option %d\n", votedOptionID+1))
+	//}
+	return sb.String()
+}
+
+func pollToString(p models.PollWithOptions) string {
+	var sb strings.Builder
+	_, _ = fmt.Fprintf(&sb, "<b>%s</b>\n", p.Title)
+	_, _ = fmt.Fprintf(&sb, "Start: %s\n", time.Unix(time.Time(p.Start).Unix(), 0))
+	_, _ = fmt.Fprintf(&sb, "Finish: %s\n", time.Unix(time.Time(p.Finish).Unix(), 0))
+	if timeToGo := time.Time(p.Finish).Sub(time.Now()); timeToGo > 0 {
+		_, _ = fmt.Fprintf(&sb, "%d days %d hours to go\n", int(timeToGo/3600)/24, int(timeToGo/3600)%24)
+	} else {
+		_, _ = fmt.Fprintf(&sb, "Poll has ended %s\n", time.Unix(time.Time(p.Finish).Unix(), 0))
+	}
+
+	for i, op := range p.Options {
+		_, _ = fmt.Fprintf(&sb, "	%d. %s\n", i+1, op.Title)
+	}
+
+	return sb.String()
 }
